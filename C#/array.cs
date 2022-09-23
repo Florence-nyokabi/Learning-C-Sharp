32c8beab4251f7ff26fb9d82a111d2dbae699b37
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-23 21:37:40 +0300

    System.Linq Namespace

---
+++ C#/array.cs
@@ -0,0 +1,18 @@
+// array methods, such as Min, Max, and Sum, can be found in the System.Linq namespace
+
+using System;
+using System.Linq;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] myNumbers = {5, 1, 8, 9};
+      Console.WriteLine(myNumbers.Max());  // returns the largest value
+      Console.WriteLine(myNumbers.Min());  // returns the smallest value
+      Console.WriteLine(myNumbers.Sum());  // returns the sum of elements
+    }
+  }
+}
