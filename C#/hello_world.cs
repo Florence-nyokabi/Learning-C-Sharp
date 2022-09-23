e389baf578f06a85bf885d3abc35a35191850f81
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-23 14:21:56 +0300

    Hello world!

---
+++ C#/hello_world.cs
@@ -0,0 +1,13 @@
+// Namespce declaration
+using System;
+
+class Hello
+{
+    static void Main() // main method is the entry point of the application
+    {
+        Console.WriteLine("Hello, World");
+    }
+}
+
+/* output
+Hello, World! */
