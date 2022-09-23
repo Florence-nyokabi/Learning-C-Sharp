a8036c27848a4825a63938fea11f83b8135622cd
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-23 21:40:49 +0300

    Arrays

--- C#/array.cs
+++ C#/array.cs
@@ -1,18 +1,34 @@
 // array methods, such as Min, Max, and Sum, can be found in the System.Linq namespace
 
 using System;
-using System.Linq;
-
-namespace MyApplication
+using System.Linq
+class Program
 {
-  class Program
-  {
-    static void Main(string[] args)
+    public static void Main()
     {
-      int[] myNumbers = {5, 1, 8, 9};
-      Console.WriteLine(myNumbers.Max());  // returns the largest value
-      Console.WriteLine(myNumbers.Min());  // returns the smallest value
-      Console.WriteLine(myNumbers.Sum());  // returns the sum of elements
+        // Initialize and assign values in different lines
+        int[] EvenNumbers = new int[3];
+        EvenNumbers[0] = 0;
+        EvenNumbers[1] = 2;
+        EvenNumbers[2] = 4;
+
+        // Initialize and assign values in the same line
+        int[] OddNumbers = { 1, 3, 5};
+
+        Console.WriteLine("Printing EVEN Numbers");
+
+        // Retrieve and print even numbers from the array
+        for (int i = 0; i < EvenNumbers.Length; i++)
+        {
+            Console.WriteLine(EvenNumbers[i]);
+        }
+
+        Console.WriteLine("Printing ODD Numbers");
+
+        // Retrieve and print odd numbers from the array
+        for (int i = 0; i < OddNumbers.Length; i++)
+        {
+            Console.WriteLine(OddNumbers[i]);
+        }
     }
-  }
 }
