8f237186b48eed31e7a0a852bf33478227f20c2c
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-23 14:18:40 +0300

    Reading and writing to console.

---
+++ C#/console.cs
@@ -0,0 +1,26 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+       // Prompting the user
+            Console.WriteLine("What is your firstName?");
+            // Read the name from console
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("What is yoour secondName?");
+            string secondName = Console.ReadLine();
+
+            // concatenate name and greeting
+            Console.WriteLine("Hello " + firstName + " " + secondName);
+            Console.ReadLine();
+    }
+}
+
+// Output
+What is your firstName?
+Florence
+What is your secondName?
+Nyokabi
+
+Hello Florence Nyokabi
