a3cd7eb7d08cb0ef312788121b5215221b36547c
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-23 21:18:24 +0300

    The default Keyword

---
+++ C#/switch_default.cs
@@ -0,0 +1,14 @@
+int day = 4;
+switch (day) 
+{
+  case 6:
+    Console.WriteLine("Today is Saturday.");
+    break;
+  case 7:
+    Console.WriteLine("Today is Sunday.");
+    break;
+  default:
+    Console.WriteLine("Looking forward to the Weekend.");
+    break;
+}
+// Outputs "Looking forward to the Weekend."
