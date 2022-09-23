e0f8c50e9a00090783594f0d69a9f8b80f63d7d5
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-23 20:43:57 +0300

    Data types.

--- C#/data_type.cs
+++ C#/data_type.cs
@@ -1,7 +1,20 @@
 int myNum = 5;                          // Integer (whole number) (4 bytes)
+Console.WriteLine(myNum);
+
 long myLong = 9,223,372,036,854,775,807 // long (whole numbers) (8 bytes)
+Console.WriteLine(myLong);
+
 double myDoubleNum = 5.99D;             // Floating point number  (8 bytes)
+Console.WriteLine(myDoubleNum;
+
 float myFloat =    0.33                 // Floating point number (4 bytes)
+Console.WriteLine(myFloat);
+
 char myLetter = 'D';                    // Character (2 bytes)
+Console.WriteLine(myLetter);
+
 bool myBool = true;                     // Boolean (1 bit)
+Console.WriteLine(myBool);
+
 string myText = "Hello";                // String (2 bytes per char)
+Console.WriteLine(myText);
