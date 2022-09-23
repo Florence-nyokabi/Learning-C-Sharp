dbf29e15932389d968b656522d9b558b592665b9
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-23 20:54:03 +0300

    Explicit casting (manually)

---
+++ C#/explicit_casting.cs
@@ -0,0 +1,8 @@
+// converting a larger type to a smaller size type (manually)
+// must be done manually by placing the type in parentheses in front of the value:
+
+double myDouble = 9.78;
+int myInt = (int) myDouble;    // Manual casting: double to int
+
+Console.WriteLine(myDouble);   // Outputs 9.78
+Console.WriteLine(myInt);      // Outputs 9
