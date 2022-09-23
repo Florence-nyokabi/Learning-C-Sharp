646aaa04043af662897d6ff17c7decbb6e11fc9a
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-23 20:51:03 +0300

    Implicit casting (automatically)

---
+++ C#/implicit_casting.cs
@@ -0,0 +1,7 @@
+// converting a smaller type to a larger type size (automatically)
+
+int myInt = 9;
+double myDouble = myInt;       // Automatic casting: int to double
+
+Console.WriteLine(myInt);      // Outputs 9
+Console.WriteLine(myDouble);   // Outputs 9
