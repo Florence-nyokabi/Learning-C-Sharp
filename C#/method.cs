ad27ecfed9426b70b8ddcef112bd4d6068898bc3
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-24 14:30:28 +0300

    Call a Method

---
+++ C#/method.cs
@@ -0,0 +1,15 @@
+static void MyMethod() 
+{
+  Console.WriteLine("I just got executed!");
+}
+
+static void Main(string[] args)
+{
+  MyMethod();
+  MyMethod();
+  MyMethod();
+}
+
+// I just got executed!
+// I just got executed!
+// I just got executed!
