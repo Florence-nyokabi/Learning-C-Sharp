11215aa3a0a3427a2a0104dbaa63579ca4ed12e8
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-09-24 14:35:39 +0300

    Parameters and Arguments

---
+++ C#/method_parameter.cs
@@ -0,0 +1,15 @@
+static void MyMethod(string fname) 
+{
+  Console.WriteLine(fname + " Wangui");
+}
+
+static void Main(string[] args)
+{
+  MyMethod("Flo");
+  MyMethod("Jenny");
+  MyMethod("Ana");
+}
+
+// Flo Wangui
+// Jenny Wangui
+// Ana Wangui
